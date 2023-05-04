866f1f0a2af26bc6db6a16fb2ea55b6e671676de
okabe-yuya <takamizawa46@gmail.com>
2023-05-04 10:02:46 +0900

    refactor: ABさんのリファクタリングを反映
    以下を参考にコードを変更しました。
    https://twitter.com/ababupdownba/status/1653534552617525248?s=20

--- src/Main.elm
+++ src/Main.elm
@@ -4,7 +4,6 @@ import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput, onClick)
-import String exposing (toInt)
 import List.Extra exposing (removeAt)
 
 main =
@@ -20,72 +19,84 @@ type alias Todo =
   , estimate : Int
   }
 
+updateTitle : String -> Todo -> Todo
+updateTitle newTitle todo =
+  { todo | title = newTitle }
+
+updateDescription : String -> Todo -> Todo
+updateDescription newDescription todo =
+  { todo | description = newDescription }
+
+updateEstimate : Int -> Todo -> Todo
+updateEstimate newEstimate todo =
+  { todo | estimate = newEstimate }
+
+
+newTodoValidate : Todo -> Bool
+newTodoValidate todo =
+  String.isEmpty todo.title || String.isEmpty todo.description
+
+
 type alias Model =
   { task : List Todo
-  , inputs : Todo
+  , newTodo : Todo
   , isValid : Bool
+  }
+
+emptyInput : Todo
+emptyInput =
+  { title = ""
+  , description = ""
+  , estimate = 0
   }
 
 init : Model
 init =
-  Model [] (Todo "" "" 0) True
+  { task = []
+  , newTodo = emptyInput
+  , isValid = True
+  }
 
 
 type Msg
   = AddTodo
   | DeleteTodo Int
-  | ChangeTitle String
-  | ChangeDescription String
-  | ChangeEstimate String
+  | UpdateTitle String
+  | UpdateDescription String
+  | UpdateEstimate String
 
 
 update : Msg -> Model -> Model
 update msg model =
+  let
+    newTodo =
+      model.newTodo
+  in
   case msg of
-    AddTodo ->
-      if String.isEmpty model.inputs.title || String.isEmpty model.inputs.description then
-        { model | isValid = False }
-      else
+      AddTodo ->
+        if newTodoValidate model.newTodo then
+          { model | isValid = False }
+        else
+          { model | newTodo = emptyInput
+                , task = newTodo :: model.task
+                , isValid = True
+                }
+
+      DeleteTodo idx ->
+        { model | task = removeAt idx model.task }
+
+      UpdateTitle newTitle ->
+        { model | newTodo = updateTitle newTitle newTodo, isValid = True }
+
+      UpdateDescription newDescription ->
+        { model | newTodo = updateDescription newDescription newTodo, isValid = True }
+
+      UpdateEstimate newEstimateText ->
         let
-          oldInputs = model.inputs
-          task = Todo oldInputs.title oldInputs.description oldInputs.estimate
-          newInputs =
-            { oldInputs | title = "", description = "", estimate = 0 }
+          newEstimate =
+            Maybe.withDefault 0 <| String.toInt newEstimateText
         in
-          { model | inputs = newInputs
-                  , task = task :: model.task
-                  , isValid = True
-                  }
-
-    DeleteTodo idx ->
-      { model | task = (removeAt idx model.task) }
-
-    ChangeTitle newTitle ->
-      let
-        oldInputs = model.inputs
-        newInputs =
-          {  oldInputs | title = newTitle }
-      in
-        { model | inputs = newInputs, isValid = True }
-
-    ChangeDescription newDescription ->
-      let
-        oldInputs = model.inputs
-        newInputs =
-          {  oldInputs | description = newDescription }
-      in
-        { model | inputs = newInputs, isValid = True }
-
-    ChangeEstimate newEstimate ->
-      let
-        toInt = case String.toInt newEstimate of
-                  Just n -> n
-                  Nothing -> 0
-        oldInputs = model.inputs
-        newInputs =
-          {  oldInputs | estimate = toInt }
-      in
-        { model | inputs = newInputs }
+        { model | newTodo = updateEstimate newEstimate newTodo }
 
 
 view : Model -> Html Msg
@@ -96,38 +107,38 @@ view model =
         , p [] [ text "タスク名" ]
         , input [ type_ "text"
                 , placeholder "ご飯を作る"
-                , value model.inputs.title
-                , onInput ChangeTitle
+                , value model.newTodo.title
+                , onInput UpdateTitle
                 ] []
-        , stringValidation model.isValid model.inputs.title "タスク名を入力してください"
+        , stringValidation model.isValid model.newTodo.title "タスク名を入力してください"
         , p [] [ text "タスクの詳細" ]
         , textarea [ placeholder "美味しいオムライスを作って食べること"
-                   , value model.inputs.description
-                   , onInput ChangeDescription
+                   , value model.newTodo.description
+                   , onInput UpdateDescription
                    ] []
-        , stringValidation model.isValid model.inputs.description "タスクの詳細を入力してください"
+        , stringValidation model.isValid model.newTodo.description "タスクの詳細を入力してください"
         , p [] [ text "Estimate" ]
         , input [ type_ "number"
                 , placeholder "Estimate"
-                , value (String.fromInt model.inputs.estimate)
+                , value (String.fromInt model.newTodo.estimate)
                 , Html.Attributes.min "0"
-                , onInput ChangeEstimate
+                , onInput UpdateEstimate
                 ] []
         , button [ onClick AddTodo ] [ text "追加" ]
       ]
       , h2 [] [ text "タスクの一覧" ]
-      , div [] (List.indexedMap (\i todo -> todoList todo i) model.task)
+      , div [] <| List.indexedMap (\i todo -> todoList todo i) model.task
     ]
 
 
 todoList : Todo -> Int -> Html Msg
 todoList todo idx =
   div []
-  [ h3 [] [ text ("タスク名: " ++ todo.title) ]
-  , p [] [ text ("タスクの詳細: " ++ todo.description) ]
-  , p [] [ text ("Estimate: " ++ String.fromInt todo.estimate) ]
-  , button [ onClick (DeleteTodo idx) ] [ text "削除" ]
-  ]
+    [ h3 [] [ text ("タスク名: " ++ todo.title) ]
+    , p [] [ text ("タスクの詳細: " ++ todo.description) ]
+    , p [] [ text ("Estimate: " ++ String.fromInt todo.estimate) ]
+    , button [ onClick (DeleteTodo idx) ] [ text "削除" ]
+    ]
 
 
 stringValidation : Bool -> String -> String -> Html Msg
